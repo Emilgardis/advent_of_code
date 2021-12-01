891e25e084e39e59ce37542c29f66096a87708c7
Emil Gardström <emil.gardstrom@gmail.com>
2021-12-01 10:33:58 +0100

    don't do main.rs

--- 2020/day01-report_repair/src/main.rs
+++
@@ -1,8 +0,0 @@
-use y2020_day1_report_repair::Solution;
-use aoc::{Aoc, parts::*};
-
-pub fn main() -> Result<(), eyre::Report> {
-    color_eyre::install()?;
-    println!("⭐Solution found⭐ ::\n{}", Aoc::solve::<Solution, Year2020, Day1, Part1>()?);
-    Ok(())
-}
